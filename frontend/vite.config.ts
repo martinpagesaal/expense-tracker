e7231703347fd20482c5d98bc9294d6318609b2e
Martin Page <martin.page@experoinc.com>
2026-01-30 13:23:10 -0300

    Deployment dev yaml

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+//https://vite.dev/config/
 const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
 const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
 
